06f70b1c8a38083ab909e2688d53c43394fe99d2
Ziad <mr.ziad.abdo@gmail.com>
2014-10-15 10:02:16 -0700

    *added a bunch of styling changes    *tweak styling to make it less boring    *refactor some css    *added background image

--- routes/main.rb
+++ routes/main.rb
@@ -4,3 +4,14 @@ get '/' do
   @all_contacts = ["name" => "Alexander"]
   erb :index
 end
+
+post '/contacts/create' do
+  requests = SendHubParty.new
+  response = requests.new_contact(params['name'], params['phone'])
+  response.body
+end
+
+post '/contacts/send_message' do
+  request = SendHubParty.new
+  response = request.send_message(params['contactid'], params['message'])
+end
